05ba697c45506b488c3bb745487da3a55862729b
Phaneendra <phaniguttula25@gmail.com>
2024-07-01 13:10:24 +0530

    working on arrays

--- Day-8 Arrays/src/com/learning/java/arrays/ArrayReverse.java
+++ Day-8 Arrays/src/com/learning/java/arrays/ArrayReverse.java
@@ -4,40 +4,11 @@ public class ArrayReverse {
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5};
 
-        // Print the original array
-        System.out.println("Original array:");
-        printArray(array); // static dont need object
+        int n;
+        n = array.length;
 
-        // Reverse the array
-        reverse(array);
-
-        // Print the reversed array
-        System.out.println("Reversed array:");
-        printArray(array);
-    }
-
-    // Method to reverse the array
-    public static void reverse(int[] array) {
-        int start = 0;
-        int end = array.length - 1;
-
-        while (start < end) {
-            // Swap the elements at start and end
-            int temp = array[start];
-            array[start] = array[end];
-            array[end] = temp;
-
-            // Move the start index forward and end index backward
-            start++;
-            end--;
+        for (int i = n - 1; i >= 0; i--){
+            System.out.print(array[i] + " ");
         }
-    }
-
-    // Method to print the array
-    public static void printArray(int[] array) {
-        for (int element : array) {
-            System.out.print(element + " ");
-        }
-        System.out.println();
     }
 }
--- Day-8 Arrays/src/com/learning/java/arrays/SmallestOrLargest.java
+++ Day-8 Arrays/src/com/learning/java/arrays/SmallestOrLargest.java
@@ -1,17 +1,24 @@
 package com.learning.java.arrays;
 
-//Write a java program to print the largest and smallest
-//element in an Unsorted Array
+/*Write a java program to print the largest and smallest
+element in an Unsorted Array */
 
 public class SmallestOrLargest {
-    int[] array = {12, 35, 1, 10, 34, 1};
+    public static void main(String[] args) {
+        int[] array = {12, 35, 1, 10, 34, 1};
 
-    int smallest = array[0];
-    int largest = array[0];
+        int smallest = array[0];
+        int largest = array[0];
 
-    for(int i = 1; i < array.length ; i++){
-
+        for (int i = 1; i < array.length; i++) {
+            if (smallest > array[i])
+                smallest = array[i];
+            if (largest < array[i])
+                largest = array[i];
+        }
+        System.out.println("Smallest Number : " +smallest);
+        System.out.println("Largest Number : " + largest);
     }
 
-
 }
+
